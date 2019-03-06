09d00658c921b7d45b36a5ab475bbbebd89edf16
Maxim Ulyanov <maxi@maxism.ru>
2019-03-06 17:30:08 +0300

    config api url with .env file

--- wkeeper-front/src/main.ts
+++ wkeeper-front/src/main.ts
@@ -4,12 +4,15 @@ import store from './store';
 import router from './router';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+const defaultApiURL = 'http://localhost:3000/api';
+const envApiURL = process.env.VUE_APP_API_URL;
+
+axios.defaults.baseURL = envApiURL || defaultApiURL;
 
 Vue.config.productionTip = false;
 
 new Vue({
-    render: (h) => h(App),
-    router,
-    store
+  render: h => h(App),
+  router,
+  store,
 }).$mount('#app');
